e97aae0042bf77f9891c864d7519c1e6d83f4952
alext8383 <76703758+alext8383@users.noreply.github.com>
2020-12-30 16:48:30 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+let mySprite = sprites.create(img`
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    fffffffffff5ffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    fff1ffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffff.fffff
+    fffff2ffffff4fffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffff7fffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffff3fffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffff8fffffffff9fffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffbfffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    fffff6ffffffffffffffffffffffffff
+    ffafffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffefffffdfffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffcfffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    ffffffffffffffffffffffffffffffff
+    `, SpriteKind.Player)
